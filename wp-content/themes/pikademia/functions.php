68fa9430b8810de9440917395284bf9bbe6044a1
Marek Zientek <marek.zientek@drupeo.pl>
2024-02-09 12:23:01 +0100

    Dodano dostawowe pliki skórki oraz podstawową konfiguracje

--- wp-content/themes/pikademia/functions.php
+++ wp-content/themes/pikademia/functions.php
@@ -1,1 +1,7 @@
 <?php
+
+function add_theme_scripts() {
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css') );
+}
+
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
